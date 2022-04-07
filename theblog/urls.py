156afe3b4d5dbf29800d547790c25001e6733aff
trigg8 <tannerrigg@gmail.com>
2022-04-07 15:12:40 +0700

    added add post page

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView
+from .views import AddPostView, HomeView, ArticleDetailView
 
 urlpatterns = [
     #path('', views.home, name='home'),
     path('', HomeView.as_view(), name= "home"),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail')
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
 ]
