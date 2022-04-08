d98b9dc8226e612f6a97ce847a84487400b14b77
trigg8 <tannerrigg@gmail.com>
2022-04-08 19:39:35 +0700

    added profile association and bio

--- theblog/models.py
+++ theblog/models.py
@@ -15,6 +15,14 @@ class Category(models.Model):
         #return reverse('article-detail', args=(str(self.id)))
         return reverse('home')
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     header_image = models.ImageField(null=True, blank=True, upload_to='images/')
@@ -35,3 +43,6 @@ class Post(models.Model):
     def get_absolute_url(self):
         #return reverse('article-detail', args=(str(self.id)))
         return reverse('home')
+
+
+
