d5b1d73a2d8ce06e4e0923a8e0202593c0a00287
trigg8 <tannerrigg@gmail.com>
2022-04-09 21:41:35 +0700

    Add heroku settings to production

--- djangoblog/settings/prod.py
+++ djangoblog/settings/prod.py
@@ -11,5 +11,36 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    ''
+    'tannerdjangoblog.herokuapp.com'
 ]
+
+DEBUG_PROPGATE_EXCEPTIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'MYAPP': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+django_on_heroku.settings(locals(), staticfiles=False)
+del DATABASES['defaults']['OPTIONS']['sslmode']
