ec48a3c2f6997d8988faea3431d26e833d84db19
trigg8 <tannerrigg@gmail.com>
2022-04-09 12:34:19 +0700

    Created create user profile page and bootstrapped it and the edit profile page

--- members/views.py
+++ members/views.py
@@ -1,17 +1,30 @@
 from theblog.models import Profile
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
-from .forms import EditProfileForm, SignUpForm, PasswordChangingForm
+from .forms import EditProfileForm, ProfilePageForm, SignUpForm, PasswordChangingForm
+
+
+class CreateProfilePageView(CreateView):
+    model = Profile
+    form_class = ProfilePageForm
+    template_name = "registration/create_user_profile_page.html"
+    #fields = '__all__'
+
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
 class EditProfilePageView(generic.UpdateView):
     model = Profile
+    form_class = ProfilePageForm
     template_name = 'registration/edit_profile_page.html'
-    fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'github_url']
+    #fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url', 'github_url']
     success_url = reverse_lazy('home')
 
 
